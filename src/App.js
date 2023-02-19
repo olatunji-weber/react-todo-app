15802e16fe453b8fe9276899716345c1290f6088
olatunji-weber <olatunji.weber@gmail.com>
2023-02-19 12:27:56 +0200

    This is another commit for the Todo App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+// import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 
+let num1 = 500;
+let num2 = 600
+
 function App() {
+  const [todos, setTodos] = useState(["Play Violin", "Practice Coding", "Take an Online Class"]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Programming is so much fun.....</h1>
-        <p>Hope you will agree with me.</p>
+        <h1>Welcome to Coder Republik</h1>
+        <p>Programming is the superpower of the 21st Century....</p>
+        <input id='todo' name='todo' />
+        <button>Add Todo</button>
+        <hr />
+        <ul>
+          {todos.map((todo)=>(
+            <li>{todo}</li>
+          ))}          
+        </ul>
       </header>
     </div>
   );
