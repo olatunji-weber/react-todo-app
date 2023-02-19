0a2f663a89a6467bc15fad1f94d7553ee41b4f12
olatunji-weber <olatunji.weber@gmail.com>
2023-02-19 13:15:36 +0200

    This is another commit still for the Todo App

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ let num1 = 500;
 let num2 = 600
 
 function App() {
-  const [todos, setTodos] = useState(["Play Violin", "Practice Coding", "Take an Online Class"]);
+  const [todos, setTodos] = useState(["Really Cool Stuff....", "Play Violin", "Practice Coding", "Take an Online Class"]);
+  const [input, setInput] = useState('');
+  const addTodo = (event) => {
+    setTodos([...todos, input]);
+    setInput('');
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Coder Republik</h1>
         <p>Programming is the superpower of the 21st Century....</p>
-        <input id='todo' name='todo' />
-        <button>Add Todo</button>
-        <hr />
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
         <ul>
           {todos.map((todo)=>(
             <li>{todo}</li>
